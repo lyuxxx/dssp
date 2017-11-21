db2f2a9e14c7db0371be6d1b91c21ed02f5ebd8b
qinbo <2654390703@qq.com>
2017-11-21 15:28:31 +0800

    登录页面修改

--- dssp/Login/Controller/LoginViewController.m
+++ dssp/Login/Controller/LoginViewController.m
@@ -71,7 +71,7 @@
     [_skipBtn setTitleColor:[UIColor colorWithHexString:GeneralColorString] forState:UIControlStateNormal];
     [self.view addSubview:_skipBtn];
     [_skipBtn makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(44 * HeightCoefficient);
+        make.top.equalTo(22 * HeightCoefficient+kStatusBarHeight);
         make.right.equalTo(-18 * WidthCoefficient);
         make.width.equalTo(50 * WidthCoefficient);
          make.height.equalTo(20 * HeightCoefficient);
@@ -283,6 +283,40 @@
         self.passWordField.secureTextEntry = !sender.selected;
     }
     if(sender == self.authBtn) {
+      
+        __block NSInteger time = 59; //倒计时时间
+        
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+        
+        dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
+        
+        dispatch_source_set_event_handler(_timer, ^{
+            
+            if(time <= 0){ //倒计时结束，关闭
+                
+                dispatch_source_cancel(_timer);
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    //设置按钮的样式
+                    [self.authBtn setTitle:@"重新发送" forState:UIControlStateNormal];
+                    [self.authBtn setTitleColor:[UIColor colorWithHexString:@"c4b7a6"] forState:UIControlStateNormal];
+                     [_authBtn.titleLabel setFont:[UIFont fontWithName:FontName size:11]];
+                    self.authBtn.userInteractionEnabled = YES;
+                });
+                
+            }else{
+                int seconds = time % 60;
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    //设置按钮显示读秒效果
+                    [self.authBtn setTitle:[NSString stringWithFormat:@"重新发送(%.2d)", seconds] forState:UIControlStateNormal];
+                    [self.authBtn setTitleColor:[UIColor colorWithHexString:@"c4b7a6"] forState:UIControlStateNormal];
+                    [_authBtn.titleLabel setFont:[UIFont fontWithName:FontName size:11]];
+                    self.authBtn.userInteractionEnabled = NO;
+                });
+                time--;
+            }
+        });
+        dispatch_resume(_timer);
         
        
     }
