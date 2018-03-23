b4ec021235d349fb84a15598e0b23b5718c9b9cc
yxliu <yuxuan.liu2@pactera.com>
2018-03-23 14:33:20 +0800

    cid10s

--- dssp/Login/Controller/LoginViewController.m
+++ dssp/Login/Controller/LoginViewController.m
@@ -652,7 +652,7 @@
 
                 {
                     MBProgressHUD *hud = [MBProgressHUD showMessage:@""];
-                    __block NSInteger time = 20;
+                    __block NSInteger time = 10;
                     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
                     cidTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
                     dispatch_source_set_timer(cidTimer,dispatch_walltime(NULL, 0), 1.0*NSEC_PER_SEC, 0);
