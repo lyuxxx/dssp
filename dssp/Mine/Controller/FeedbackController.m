b4f5f061dcc5b3d127b5719a0b045171caa41cfe
zhujilong <zhujilong2007@sina.com>
2018-05-29 14:34:12 +0800

    倒角问题修改

--- dssp/Mine/Controller/FeedbackController.m
+++ dssp/Mine/Controller/FeedbackController.m
@@ -314,18 +314,6 @@
         [MBProgressHUD showText:[NSString stringWithFormat:@"%@:%ld",NSLocalizedString(@"请求失败", nil),code]];
         callback.failure(code);
     }];
-    
-//    [CUHTTPRequest POST:testUrl parameters:paras success:^(id responseData) {
-//        NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingAllowFragments error:nil];
-//
-//        if ([[dic objectForKey:@"code"] isEqualToString:@"200"]) {
-//            callback.success(dic[@"msg"]);
-//        } else {
-//            [MBProgressHUD showText:dic[@"msg"]];
-//        }
-//    } failure:^(NSInteger code) {
-//        callback.failure(code);
-//    }];
 
 }
 
@@ -549,6 +537,8 @@
 - (FeedbackPicView *)picView {
     if (!_picView) {
         _picView = [FeedbackPicView new];
+        _picView.layer.cornerRadius = 4;
+        _picView.layer.masksToBounds = YES;
     }
     return  _picView;
 }
